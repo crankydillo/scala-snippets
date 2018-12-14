9d69906e3a619843bd56fc39edfb7bb10b7bb2a7
Samuel Cox <crankydillo@gmail.com>
2018-12-14 07:48:06 -0600

    Copy of Heiko's HTTP tracing concept (without akka-http)

---
+++ akka/akka-streams/heikos-graph/README.md
@@ -0,0 +1,4 @@
+# Introduction
+
+This is a simplified version of Heiko Seeberger's [HTTP tracelog
+example](https://youtu.be/ryxrWVI3PMA?t=1051).
---
+++ akka/akka-streams/heikos-graph/src/main/scala/App.scala
@@ -0,0 +1,83 @@
+import scala.concurrent.Future
+
+import akka.{Done, NotUsed}
+import akka.actor.ActorSystem
+import akka.stream._
+import akka.stream.scaladsl._
+
+/**
+  * A quick port of Heiko's HTTP log tracing graph without akka-http.
+  * All credit goes to him.  See README for a link to his video.
+  *
+  * Here's the graph:
+  *
+  *                   +---------------+
+  *         +-------->| businessLogic |---------+
+  *         |         +---------------+         |
+  *         |                                   |
+  *         |                                   v
+  *    +----------+        +--------+       +----------+
+  * -->| bcastStr |------->| enrich |------>| bcastInt |-->
+  *    +----------+        +--------+       +----------+
+  *                            |                |
+  *                            v                |
+  *    +---------------+   +-------+            |
+  *    | processingLog |<--|  zip  |<-----------+
+  *    +---------------+   +-------+
+  */
+object App {
+
+  // The graph implementation of the ascii art.
+  def withProcessingLog
+    (enrich: String => String)
+    (businessLogic: NormalFlow  // heiko's 'handler'
+  ): NormalFlow = {
+    Flow.fromGraph(GraphDSL.create(processingLog) { implicit builder => processingLog =>
+      import GraphDSL.Implicits._
+
+      val bcastStr = builder.add(Broadcast[String](2))
+      val bcastInt = builder.add(Broadcast[Int](2))
+      val enrichStr = builder.add(Flow[String].map(enrich))
+      val zip = builder.add(Zip[String, Int])
+
+      bcastStr ~> businessLogic ~> bcastInt
+      bcastStr ~> enrichStr ~> zip.in0
+      bcastInt.out(1) ~> zip.in1
+      zip.out ~> processingLog
+
+      FlowShape(bcastStr.in, bcastInt.out(0))
+    })
+  }
+
+
+  def main(args: Array[String]): Unit = {
+
+    implicit val system = ActorSystem("heikos-graph")
+    implicit val mat = ActorMaterializer()
+    implicit val ec = system.dispatcher
+
+    val strings = Source(List("ab", "cdef"))
+
+    def process(businessLogic: NormalFlow) =
+      strings.via(businessLogic).runForeach(println)
+
+    val done1 = process(someFlow)
+
+    val done2 = process(withProcessingLog(s => s"In: $s")(someFlow))
+
+    done1.zip(done2).onComplete(_ => system.terminate())
+  }
+
+  val someFlow: NormalFlow = Flow[String].map { _.length }
+
+  type NormalFlow = Flow[String, Int, Any]
+  type Log = Sink[(String, Int), Future[Done]]
+
+
+  val processingLog: Log = {
+    Sink.foreach {
+      case (in, out) => println(s"$in = $out")
+    }
+  }
+
+}
